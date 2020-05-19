335468ad8f8e52cb8d2850a09bce650585ccb2b9
Andre Ferreira <andre.ferreira.private@gmail.com>
2020-05-19 09:47:56 +0200

    Added status publish and wi-fi connection check

--- template_wifi_mqtt_ota.ino
+++ template_wifi_mqtt_ota.ino
@@ -11,8 +11,8 @@
  * Optional MQTT server via #define
  * 
  * Author: Andre Ferreira
- * Date: 24 March 2020
- * Built for the nodeMCU device
+ * Date: 19 May 2020
+ * Built for the ESP8266 based devices
  * 
  *****************************************************************************/
 
@@ -25,9 +25,13 @@
 #include <ArduinoOTA.h>
 #include <PubSubClient.h>
 
+
+
+
 /************************ Define device and mqtt topic and things ********/
 
 String NodeID = "device_name_goes_here";    // Define the device name (used in mqtt pub)
+String DeviceType = "device_type_goes_here";  // Define the device type for future reference
 
 //#define WIFI              // Define this if you want to use Wi-Fi
 //#define MQTT              // Define this if you want to use MQTT (Needs WIFI above)
@@ -37,13 +41,15 @@ String NodeID = "device_name_goes_here";    // Define the device name (used in m
 WiFiClient wifiClient;
 PubSubClient mqttClient(wifiClient);
 
+
 /************************* WiFi and MQTT variables ***************************/
 
-const char* ssid = "your_ssid_here";
-const char* password = "your_wifi_password_here";
-const char* mqtt_server = "your_mqtt_server_IP_here";
+const char* ssid = "kenmar";
+const char* password = "C0urtney1";
+const char* mqtt_server = "172.16.20.10";
 
-String pubTopic="home/"+NodeID+"/values";
+String pubTopic="home/"+NodeID+"/values";       // Default data topic for sensor values
+String statusTopic="home/deviceStatus";         // Default device status messages topic
 
 /************************* Declare global vars *********************************/
 
@@ -56,6 +62,8 @@ char payload [128];                  // json output for the mqtt pub
 /************************* Function to connect to MQTT *************************/
 
 void mqtt_reconnect() {
+  char statPayload[128];
+  
   // Loop until we're reconnected
   while (!mqttClient.connected()) {
     Serial.print("Attempting MQTT connection...");
@@ -64,7 +72,8 @@ void mqtt_reconnect() {
     if (mqttClient.connect(clientId.c_str())) {
       Serial.println("connected");
       // Once connected, publish an announcement...
-      mqttClient.publish(pubTopic.c_str(), "Restart..");
+      snprintf(statPayload, sizeof(statPayload), "{\"%s\":{\"DeviceType\":\"%s\",\"status\":{\"%s\":\"%s\"}}}", NodeID.c_str(), DeviceType.c_str(), "MQTT", "Connected" ); 
+      mqttClient.publish(statusTopic.c_str(), statPayload);
     } else {
       Serial.print("failed, rc=");
       Serial.print(mqttClient.state());
@@ -90,7 +99,7 @@ void callback(char* topic, byte* payload, unsigned int length) {
 }
 
 
-/************************* Function to read the sensor *************************/
+/************************* Function to read the CT *************************/
 void read_sensor()
 {
   float value1;         // Placeholders for the mqtt code below..
@@ -99,7 +108,7 @@ void read_sensor()
   // Do some clever stuff here
 
 #ifdef MQTT
-  snprintf(payload, sizeof(payload), "{\"%s\":{\"status\":{\"%s\":\"%.2f\",\"%s\":\"%.2f\"}}}", NodeID.c_str(), "val1", value1, "val2", value2 );
+  snprintf(payload, sizeof(payload), "{\"%s\":{\"DeviceType\":\"%s\",\"status\":{\"%s\":\"%.2f\",\"%s\":\"%.2f\"}}}", NodeID.c_str(), DeviceType.c_str(),"val1", value1, "val2", value2 );
   Serial.println (payload);
   Serial.println (pubTopic);
   mqttClient.publish(pubTopic.c_str(), payload);
@@ -183,7 +192,22 @@ void loop() {
   // Setup for OTA updates
   ArduinoOTA.handle();
 
+#ifdef WIFI
+ // Check for wifi disconnect and reconnect if broken
+ if (WiFi.status() != WL_CONNECTED) {
+    WiFi.mode(WIFI_STA);    // Ensure we are in Station mode
+    WiFi.begin(ssid, password);
+
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(500);
+      Serial.print(".");
+    }
+  }
+#endif
+
+
 #ifdef MQTT
+  // Check if MQTT connected and fix if broken
   if (!mqttClient.connected()) {
     mqtt_reconnect();
   }
